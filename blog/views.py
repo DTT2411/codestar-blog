3a3bd856ac783b66d3c173d77e905761c57435dd
DTT2411 <david.thomson2411@gmail.com>
2025-06-02 15:29:22 +0100

    Create new base template

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1) # Ensures only published posts appear on the live blog
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
